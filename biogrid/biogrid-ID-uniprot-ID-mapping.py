379ccc34e2c103c64a0b7a0dd725516f81eaa23f
hsiaoyi0504 <hsiaoyi0504@gmail.com>
2016-03-22 11:56:48 +0800

    finish the biogrid-ID-uniprot-ID-mapping

--- biogrid/biogrid-ID-uniprot-ID-mapping.py
+++ biogrid/biogrid-ID-uniprot-ID-mapping.py
@@ -3,7 +3,8 @@ with open('BIOGRID-IDENTIFIERS-3.4.134.tab.txt') as f:
     with open('biogrid-ID-uniprot-ID-mapping.txt','w') as f2:
         for line in f:
             if 'SWISS-PROT' in line:
-                f2.write(line)
+                value = line.split()
+                f2.write( value[0] + '\t' + value[1] + '\n' )
         f2.close()
     f.close()
 
